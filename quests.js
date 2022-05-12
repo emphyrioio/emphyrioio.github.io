2cb5e0b38a406d87f3a5cec72a06b6ce160be754
emphybrioio <105437430+emphybrioio@users.noreply.github.com>
2022-05-12 16:48:27 +0200

    Version Alpha

---
+++ quests.js
@@ -0,0 +1,20 @@
+var XMLString = `
+<quests>
+    <quest id="var1" npc="Varré le Blême" location="Nécrolimbe" link="">Parler à Varré le Blême près du site de grâce le Commencement.</quest>
+    <quest id="var2" npc="Varré le Blême" location="Nécrolimbe" link="">Après s'être rendu au Bastion de Table ronde et avant d'avoir vaincu Godrick, parler à Varré près du site de grâce le Commencement.</quest>
+    <quest id="var3" npc="Varré le Blême" location="Nécrolimbe" link="">Alternative : après avoir vaincu Godrick et avant d'aller au Bastion de la Table ronde, parler à Varré près du site de grâce le Commencement.</quest>
+    <quest id="var4" npc="Varré le Blême" location="Nécrolimbe" link="">Après s'être rendu au Bastion de la Table ronde et après avoir obtenu la rune majeure de Godrick, lire le message au sol laissé par Varré près du site de grâce le Commencement.</quest>
+    <quest id="var5" npc="Varré le Blême" location="Liurnia, contrée lacustre" link="">Parler à Varré le Blême devant l'église de la Rose pour obtenir cinq Doigts sanglants purulents.</quest>
+    <quest id="var6" npc="Varré le Blême" location="Liurnia, contrée lacustre" link="">Effectuer trois invasions de mondes d'autres joueurs (peu importe l'issue de l'invasion).</quest>
+    <quest id="var7" npc="Varré le Blême" location="Liurnia, contrée lacustre" link="">Parler à Varré le Blême devant l'église de la Rose pour obtenir la Faveur du seigneur du sang.</quest>
+    <quest id="var8" npc="Varré le Blême" location="Liurnia, contrée lacustre" link="">Trouver une servante morte et imbiber l'étoffe de son sang (l'une se trouve à la chapelle de l'Anticipation et l'autre à l'église de l'Inhibition).</quest>
+    <quest id="var9" npc="Varré le Blême" location="Liurnia, contrée lacustre" link="">Parler à Varré le Blême devant l'église de la Rose pour obtenir la Médaille de chevalier pur-sang.</quest>
+    <quest id="var10" npc="Varré le Blême" location="La Siofra, palais de Mohgwyn" link="">Utiliser la Médaille de chevalier pur-sang pour se rendre au palais de Mohgwyn.</quest>
+    <quest id="yur1" npc="Yura, chasseur de Doigts crochus" location="Nécrolimbe" link=""></quest>
+    <quest id="yur2" npc="Yura, chasseur de Doigts crochus" location="Nécrolimbe" link=""></quest>
+    <quest id="yur3" npc="Yura, chasseur de Doigts crochus" location="Nécrolimbe" link=""></quest>
+    <quest id="yur4" npc="Yura, chasseur de Doigts crochus" location="Nécrolimbe" link=""></quest>
+    <quest id="gow1" npc="Gowry le Sage" location="Caelid" link=""></quest>
+    <quest id="gow2" npc="Gowry le Sage" location="Caelid" link=""></quest>
+</quests>
+`;
